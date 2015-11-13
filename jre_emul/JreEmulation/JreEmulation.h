13375a7ba1e91d5233767aa3b78371faa2985aed
kim <kim@liao.com>
2015-11-13 12:05:48 +0800

    refactor

---
+++ jre_emul/JreEmulation/JreEmulation.h
@@ -0,0 +1,19 @@
+//
+//  JreEmulation.h
+//  JreEmulation
+//
+//  Created by 徐涛 on 11/13/15.
+//  Copyright © 2015 kingxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JreEmulation.
+FOUNDATION_EXPORT double JreEmulationVersionNumber;
+
+//! Project version string for JreEmulation.
+FOUNDATION_EXPORT const unsigned char JreEmulationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JreEmulation/PublicHeader.h>
+
+
