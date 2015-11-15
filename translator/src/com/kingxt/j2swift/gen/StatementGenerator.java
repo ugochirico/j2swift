3e4029293698c309140809bd776e0c576a47e2ff
Dylan <dylan@!liao.com>
2015-11-15 16:20:29 +0800

    add boolean visit

--- translator/src/com/kingxt/j2swift/gen/StatementGenerator.java
+++ translator/src/com/kingxt/j2swift/gen/StatementGenerator.java
@@ -11,6 +11,7 @@ import org.eclipse.jdt.core.dom.IVariableBinding;
 import com.kingxt.Options;
 import com.kingxt.j2swift.ast.Assignment;
 import com.kingxt.j2swift.ast.Block;
+import com.kingxt.j2swift.ast.BooleanLiteral;
 import com.kingxt.j2swift.ast.CStringLiteral;
 import com.kingxt.j2swift.ast.CharacterLiteral;
 import com.kingxt.j2swift.ast.Expression;
@@ -295,7 +296,9 @@ public class StatementGenerator extends TreeVisitor {
 
 	/*
 	 * (non-Javadoc)
-	 * @see com.kingxt.j2swift.ast.TreeVisitor#visit(com.kingxt.j2swift.ast.InfixExpression)
+	 * 
+	 * @see com.kingxt.j2swift.ast.TreeVisitor#visit(com.kingxt.j2swift.ast.
+	 * InfixExpression)
 	 * 
 	 * Tranlator Java Operation as < > <= >= etc...
 	 */
@@ -308,7 +311,7 @@ public class StatementGenerator extends TreeVisitor {
 				.equals(InfixExpression.Operator.NOT_EQUALS))) {
 			Expression lhs = operands.get(0);
 			Expression rhs = operands.get(1);
-			//TODO
+			// TODO
 			if (lhs instanceof StringLiteral || rhs instanceof StringLiteral) {
 				if (!(lhs instanceof StringLiteral)) {
 					// In case the lhs can't call isEqual.
@@ -347,6 +350,12 @@ public class StatementGenerator extends TreeVisitor {
 		} else {
 			// buffer.append(LiteralGenerator.generate(node.getValue()));
 		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(BooleanLiteral node) {
+		buffer.append(node.booleanValue() ? "true" : "false");
 		return false;
 	}
 }
