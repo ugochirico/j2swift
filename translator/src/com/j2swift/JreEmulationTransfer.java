cd4766431f9e4d9b9a979d0c5aba24491353638b
徐 涛 <kingxt@163.com>
2015-11-21 22:56:33 +0800

    add java exception translate

--- translator/src/com/j2swift/JreEmulationTransfer.java
+++ translator/src/com/j2swift/JreEmulationTransfer.java
@@ -5,14 +5,30 @@ public class JreEmulationTransfer {
 	public static void main(String[] args) {
 		String[] fileList = new String[] {
 				"java/lang/Boolean.java",
+				"java/lang/RuntimeException.java",
+				"java/lang/ReflectiveOperationException.java",
+				"java/lang/NoSuchFieldException.java",
+				"java/lang/IllegalArgumentException.java",
+				"java/lang/NumberFormatException.java",
+				"java/lang/NegativeArraySizeException.java",
+				"java/lang/ClassCastException.java",
+				"java/lang/Runnable.java",
+				"java/lang/Exception.java", 
+				"java/lang/RuntimeException.java",
+				"java/lang/Comparable.java",
+				"java/lang/SecurityException.java",
+				"java/lang/StringIndexOutOfBoundsException.java",
+				"java/lang/TypeNotPresentException.java",
+				"java/lang/UnsupportedOperationException.java",
+				"java/lang/ArithmeticException.java",
+				"java/lang/IndexOutOfBoundsException.java",
+				"java/lang/ArrayIndexOutOfBoundsException.java",
+				
 				"java/io/ObjectStreamException.java",
 				"java/io/InvalidObjectException.java",
 				"java/io/IOException.java", 
 				"java/io/Serializable.java",
-				"java/lang/Comparable.java", 
-				"java/lang/Runnable.java",
-				"java/lang/Exception.java", 
-				"java/lang/RuntimeException.java" };
+				 };
 		new JreEmulationTransfer().run(fileList);
 	}
 
