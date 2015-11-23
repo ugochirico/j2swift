83f24e47e7514b3406331a3bea0d8032561ec166
徐 涛 <kingxt@163.com>
2015-11-23 21:57:14 +0800

    fix error compile

--- translator/src/com/j2swift/test/TestGeneric.java
+++ translator/src/com/j2swift/test/TestGeneric.java
@@ -7,19 +7,6 @@ public class TestGeneric<T> {
 	}
 	
 	public static void main(String[] args) {
-		A a = new B();
-		a.doTest(new Object());
-	}
-}
 
-class A {
-	public void doTest(Object o) {
-		System.out.println("aaaaa  "+o);
-	}
-}
-
-class B extends A {
-	public void doTest(String s) {
-		System.out.println("bbbbbb  "+s);
 	}
 }
